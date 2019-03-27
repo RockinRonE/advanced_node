4613b8b0127025eefd7db2c01a9a55d94af028b7
Ronny Ewanek <ronny.ewanek@gmail.com>
2019-03-27 12:42:16 -0500

    Hook up redis with cache expiratin

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const requireLogin = require('../middlewares/requireLogin')
-
+const cleanCache = require('../middlewares/cleanCache')
 const Blog = mongoose.model('Blog')
 
 module.exports = app => {
@@ -14,30 +14,13 @@ module.exports = app => {
   })
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const redis = require('redis')
-    const redisUrl = 'redis://127.0.0.1:6379'
-    const client = redis.createClient(redisUrl)
-    const util = require('util')
-    // takes a function with a callback and returns a promise instead
-    // returns new function that's been promisified
-    client.get = util.promisify(client.get)
-
-    // Do we have any cached data in redis?
-    const cachedBlogs = await client.get(req.user.id)
-
-    if (cachedBlogs) {
-      console.log('SERVING FROM CACHED')
-      return res.send(JSON.parse(cachedBlogs))
-    }
-
-    const blogs = await Blog.find({ _user: req.user.id })
-    console.log('SERVING FROM MONGODB')
+    const blogs = await Blog.find({ _user: req.user.id }).cache({
+      key: req.user.id
+    })
     res.send(blogs)
-
-    client.set(req.user.id, JSON.stringify(blogs))
   })
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body
 
     const blog = new Blog({
